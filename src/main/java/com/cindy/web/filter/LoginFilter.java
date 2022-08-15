c363c6c00565d711e1fa01a0ee559b964ba43796
Cindiddy <101303125+Cindiddy@users.noreply.github.com>
2022-08-15 14:39:30 -0400

    add filter to make sure user login first

---
+++ src/main/java/com/cindy/web/filter/LoginFilter.java
@@ -0,0 +1,45 @@
+package com.cindy.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest req= (HttpServletRequest) request;
+
+        String[] urls={"/login.jsp","/imgs/","/css/","/loginServlet","register.jsp","/registerServlet"};
+
+        String url = req.getRequestURL().toString();
+
+        for (String u:urls){
+            if (url.contains(u)){
+                chain.doFilter(request, response);
+                return;
+            }
+        }
+
+        HttpSession session = req.getSession();
+        Object user = session.getAttribute("user");
+
+        if(user!=null){
+            chain.doFilter(request, response);
+        }else {
+            req.setAttribute("login_msg","You need to login first");
+            req.getRequestDispatcher("/login.jsp").forward(req,response);
+        }
+
+
+    }
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+
+}
